28b799889d20a86095677a16642a29f4945b0a8d
akola2017 <akola@post.pl>
2018-05-06 14:58:39 +0200

    ostatnie

--- test_ui/test_ui.py
+++ test_ui/test_ui.py
@@ -8,20 +8,23 @@ class TestFormater(unittest.TestCase):
     def test_plain_lowercase(self):
         driver = webdriver.Chrome()
         driver.get("http://127.0.0.1:5000/ui")
-        self.poprawny(driver)
-        time.sleep(5)
+        self.is_greatings_visible(driver)
+        self.is_name_present(driver)
+        self.is_google_integration_works(driver)
+        #time.sleep(5)
         driver.quit()
 
-    def poprawny(self, driver):
+    def is_greatings_visible(self, driver):
         wyswietla = driver.find_element_by_id('napis')
         print(wyswietla.text)
         self.assertEqual(wyswietla.text, "Witaj swiecie")
 
-    def gmail(self, driver):
-        driver.find_element_by_id("znani").Click()
-        self.assertEqual()
-
-    def sprawdzimie(self, driver):
+    def is_name_present(self, driver):
         wyswiet_imie = driver.find_element_by_id('imie')
         print(wyswietl_imie.text)
         self.assertEqual(wyswietl_imie.text, "AgnieszkaR")
+
+    def is_google_integration_works(self, driver):
+        driver.find_element_by_id("znani").Click()
+        #wait = WebDriverWait(driver, 2)
+        #WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, "res")))
