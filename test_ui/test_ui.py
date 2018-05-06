ff7c038512a54174cfeb364c3c8d0d70af7e994f
akola2017 <akola@post.pl>
2018-05-06 14:04:40 +0200

    zmiany

--- test_ui/test_ui.py
+++ test_ui/test_ui.py
@@ -8,5 +8,11 @@ class TestFormater(unittest.TestCase):
     def test_plain_lowercase(self):
         driver = webdriver.Chrome()
         driver.get("http://127.0.0.1:5000/ui")
-        #time.sleep(10)
+        self.poprawny(driver)
+        time.sleep(5)
         driver.quit()
+
+    def poprawny(self, driver):
+        wyswietla = driver.find_element_by_id('napis')
+        print(wyswietla.text)
+        self.assertEqual(wyswietla.text, "Witaj swiecie")
